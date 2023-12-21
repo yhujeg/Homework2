fc4316d9de9c83ce0b5cdd4c3fa279709261ee2e
yhujeg <nikita.manov10@gmail.com>
2023-12-21 19:10:33 +0300

    Homework2

---
+++ script.js
@@ -0,0 +1,83 @@
+// задание 1
+// let a = 10;
+// a = 20
+// alert (a);
+
+// задание 2
+// let releaseDate = 2007;
+// alert (`Год выпуска первого iPhone - ${releaseDate}.`);
+
+// задание 3
+// let creatorName = "Брендан Эйх";
+// alert (`Имя создателя JavaScript - ${creatorName}.`);
+
+// задание 4
+// let x = 10;
+// let y = 2;
+// let resultPlus = x + y;
+// alert (resultPlus);
+
+// let z = 10;
+// let c = 2;
+// let resultMinus = x - y;
+// alert (resultMinus);
+
+// let g = 10;
+// let j = 2;
+// let resultMultiply = g * j;
+// alert (resultMultiply);
+
+// let v = 10;
+// let p = 2;
+// let resultDivide = v / p;
+// alert (resultDivide);
+
+// задание 5
+// let h = 2;
+// let n = 5;
+// let result = h**n;
+// alert (result);
+
+// задание 6
+// let a = 9;
+// let b = 2;
+// let remainder = a % b;
+// alert (remainder);
+
+// задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+// задание 8
+// let age = prompt("Сколько вам лет?");
+// alert (`Вам ${age} лет`);
+
+// задание 9
+// const user = {
+// 	name: 'Pablo',
+// 	age: '44',
+// 	isAmdin: 'false',
+// };
+
+// задание 9.1
+// user["cityOfResidence"] = "Acapulco";
+
+// задание 9.2
+// user.age = 35;
+
+// задание 9.3
+// delete user.cityOfResidence;
+
+// задание 9.4
+// let info = prompt ("Какую информацию хотите узнать о пользователе?");
+// alert (user[info]);
+
+// задание 10
+// let name = prompt ('Как вас зовут?');
+// alert (`Привет, ${name}!`)
